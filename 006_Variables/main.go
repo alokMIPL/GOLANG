a5df6acb678e8901142e5e4600e6b77dcc27b755
alokMIPL <alok@madhulata.com>
2026-08-11 10:06:11 +0530

    variables code updated

---
+++ 006_Variables/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// basic style
+	var name string = "golang"
+	fmt.Println(name)
+
+	// short method
+	// it automatic infer or detect that the variable is string.
+
+	/*
+		var empName string
+		short method it automatic infer or detect that the variable is string.
+	*/
+
+	var empName = "John"
+	fmt.Println(empName)
+
+	// Boolean
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	// Integer
+	var age int = 12
+	fmt.Println(age)
+
+	// Now ShortHand method
+	rollNumber := "02"
+	fmt.Println(rollNumber)
+
+	studentName := "Didarian"
+	fmt.Println(studentName)
+
+}
