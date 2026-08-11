32625079e499756835bd6dece02b8b92ab7d29b4
alokMIPL <alok@madhulata.com>
2026-08-11 09:36:44 +0530

    Simple Values code updated

--- 005_Simple_Values/main.go
+++ 005_Simple_Values/main.go
@@ -18,5 +18,21 @@ func main() {
 	fmt.Println(false)
 
 	// floats
+	fmt.Println(10.4)
+
+	// Arithmatic Operations
+	fmt.Println(7.0 / 3.5)
 
 }
+
+/*
+Output
+1
+2
+Hello Go
+true
+false
+10.4
+2
+
+*/
