0987473ec9f9b89d642c4bf06b575b4c011825dd
alokMIPL <alok@madhulata.com>
2026-08-12 16:16:20 +0530

    code added

--- 011_Array/main.go
+++ 011_Array/main.go
@@ -12,13 +12,27 @@ func main() {
 	fmt.Println(len(nums))
 	// Output: 4
 
+	fmt.Println("*****************")
+
 	// Add element in array at particular INDEX
 	nums[0] = 1
 	fmt.Println(nums[0])
 	// Output: 1
 
+	fmt.Println("*****************")
+
 	// Printing the whole array
 	fmt.Println(nums)
 	// Output: [1 0 0 0]
+	// As we see the array have only one index[0] vlaue initialized and rest for index are not initalized by any valuye so, in GOLANG it does not store Garbage value to it. It Store 0 0 0 for index[1], index[2] and index[3]
+
+	fmt.Println("*****************")
+
+	// Same for Boolean also
+	var vals [4]bool
+	fmt.Println(vals)
+	// Output: [false false false false]
+
+	fmt.Println("*****************")
 
 }
