5f1ca4e5fa88f869f7dde108e98256d39af8be71
alokMIPL <alok@madhulata.com>
2026-08-12 14:44:22 +0530

    code added

--- 010_switch/main.go
+++ 010_switch/main.go
@@ -6,11 +6,25 @@ func main() {
 
 	// When we have too much conditions then we use SWITCH.
 
+	// So Simple Switch
+
 	i := 5
 
 	switch i {
 	case 1:
 		fmt.Println("One")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	case 5:
+		fmt.Println("five")
+	default:
+		fmt.Println("other")
 	}
+
+	// Multiple condition Switch
 
 }
