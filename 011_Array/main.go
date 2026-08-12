fe29509a8c3d1c235d0ab4b150489eefc88cca40
alokMIPL <alok@madhulata.com>
2026-08-12 15:29:28 +0530

    code added

---
+++ 011_Array/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Array is Number sequence of specific length
+func main() {
+
+	// Declaration of Array
+	var nums [4]int
+
+	// Finding the lenght of Array
+	fmt.Println(len(nums))
+	// Output: 4
+
+}
