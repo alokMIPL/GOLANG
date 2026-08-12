a1d2d6d6470785957b4d0f71548d8834db91d4ad
alokMIPL <alok@madhulata.com>
2026-08-12 15:52:13 +0530

    code added

--- 011_Array/main.go
+++ 011_Array/main.go
@@ -12,4 +12,13 @@ func main() {
 	fmt.Println(len(nums))
 	// Output: 4
 
+	// Add element in array at particular INDEX
+	nums[0] = 1
+	fmt.Println(nums[0])
+	// Output: 1
+
+	// Printing the whole array
+	fmt.Println(nums)
+	// Output: [1 0 0 0]
+
 }
