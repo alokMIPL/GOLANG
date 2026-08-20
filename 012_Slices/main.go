40436c796c24482c95cbb563cf457fca0201122c
alokMIPL <alok@madhulata.com>
2026-08-20 17:57:15 +0530

    code added

--- 012_Slices/main.go
+++ 012_Slices/main.go
@@ -30,8 +30,26 @@ func main() {
 	// var num = make([]int, 10)
 	// Output: [0 0]
 
+	// Now When we declare the size and initialized or even not initialize the value then it show Value 0 0 0 0 not NIL.
+
 	var num = make([]int, 10)
 	// Output: [0 0 0 0 0 0 0 0 0 0]
 	fmt.Println(num)
+
+	// NOTE:  in this make([]int, 2 or 10)
+	// Just Show the limit of this slice, it doesn't mean more than 2 elements not store. SLice can store more then 2 element in this num slice.
+
+	// How to check capacity on slice
+	fmt.Println(cap(num))
+	// Output: 10
+
+	// So in SLICE make() function it have three things
+
+	// var num = make(int[], 2, 10)
+
+	// inside make() function 3 things
+	// 1. int[] = inform about the dataType in SLICE.
+	// 2. 2 = inform about the LIMIT of the SLICE.
+	// 3. 10 = inform about the CAPACITY of the SLICE.
 
 }
