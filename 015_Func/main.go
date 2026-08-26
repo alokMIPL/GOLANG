6753b6203be7fac50b1b05f5af908a1003231b66
alokMIPL <alok@madhulata.com>
2026-08-26 17:42:59 +0530

    code added

--- 015_Func/main.go
+++ 015_Func/main.go
@@ -34,6 +34,20 @@ func getLanguagesMixed() (string, string, bool) {
 
 // To compress any compiler error we use "_" to supress that error.
 
+// GOLANG functions are first class citizen functions() and can be used or assigned those functions to any variable and pass those functions as arguments for other functions.
+
+func apply(a, b int, op func(int, int) int) int {
+	return op(a, b)
+}
+
+func adds(x, y int) int {
+	return x + y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
 func main() {
 	result := add(3, 5)
 	fmt.Println(result)
@@ -43,5 +57,17 @@ func main() {
 
 	fmt.Println(getLanguagesMixed())
 	// Output = golang javaScript true
+
+	// Assign a function to a variable
+	greet := func(name string) string {
+		return "Hello, " + name
+	}
+
+	fmt.Println(greet("Alok"))
+	// Output = Hello, Alok
+
+	fmt.Println(apply(3, 4, add))
+	fmt.Println(apply(3, 4, multiply))
+	// Output = 7 12
 
 }
