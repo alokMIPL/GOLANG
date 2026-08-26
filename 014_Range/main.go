6fc7139dd292814329953498acfa211cfeadf056
alokMIPL <alok@madhulata.com>
2026-08-26 16:49:35 +0530

    code added

--- 014_Range/main.go
+++ 014_Range/main.go
@@ -27,8 +27,11 @@ func main() {
 	fmt.Println(sum)
 	// Output = 21
 
+	fmt.Println("***********")
+
 	// Now to print number with INDEX so _, is replaced by i and i is INDEX in RANGE.
 
+	// Now to access SLICE by RANGE
 	for i, num := range nums {
 		fmt.Println(num, i)
 	}
@@ -37,7 +40,49 @@ func main() {
 	6 0
 	7 1
 	8 2
+	*/
 
+	fmt.Println("***********")
+
+	// Now access MAP by using RANGE
+
+	m := map[string]string{"fname": "John", "lname": "doe"}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("***********")
+
+	// Now access String by using RANGE
+	// Basically give UNICODE
+
+	for i, c := range "golang" {
+		fmt.Println(i, c)
+	}
+
+	/* Output =
+	0 103
+	1 111
+	2 108
+	3 97
+	4 110
+	5 103
+	*/
+
+	// Now getting each and every charater of STRING then use string() function.
+
+	for i, c := range "golang" {
+		fmt.Println(i, string(c))
+	}
+
+	/*
+		0 g
+		1 o
+		2 l
+		3 a
+		4 n
+		5 g
 	*/
 
 }
