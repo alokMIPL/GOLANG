70105d66d819f89b675a4e8323b7116910188755
alokMIPL <alok@madhulata.com>
2026-08-31 09:53:07 +0530

    code added

--- 019_Struct/main.go
+++ 019_Struct/main.go
@@ -35,4 +35,19 @@ func main() {
 	fmt.Println("Order Struct", myOrder.id)
 	// Output = Order Struct 1
 
+	myOrder2 := order{
+		id:        "2",
+		amount:    40.00,
+		status:    "delivered",
+		createdAt: time.Now(),
+	}
+
+	fmt.Println("Order2 Struct", myOrder2)
+	// Output = {2 40 delivered {14023467503996331940 669301 0x7ff631305440}}
+
+	// Now see i made two instances myOrder and myOrder2. Now i want to change the myOrder instance value.
+	myOrder.status = "paid"
+	fmt.Println("Order Struct", myOrder)
+	// Output = Order Struct {1 50 paid {14023467791572242772 1 0x7ff714175440}}
+
 }
