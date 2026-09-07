b2f98d11bdfb9b9d2cda21123b86c5ccbe7ca4b9
alokMIPL <alok@madhulata.com>
2026-09-07 15:16:51 +0530

    Code added

--- 021_Interface/main.go
+++ 021_Interface/main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 type payment struct {
+	gateway stripe
 }
 
 func (p payment) makePayment(amount float32) {
 	razorpayPaymentGw := razorpay{}
 	razorpayPaymentGw.pay(amount)
+
+	stripePaymentGw := stripe{}
+	stripePaymentGw.pay(amount)
+
+	p.gateway.pay(amount)
 }
 
 type razorpay struct{}
@@ -16,7 +22,19 @@ func (r razorpay) pay(amount float32) {
 	fmt.Println("Making payment using razorpay", amount)
 }
 
+type stripe struct{}
+
+func (s stripe) pay(amount float32) {
+	fmt.Println("Making payment using stripe", amount)
+}
+
 func main() {
-	newPayment := payment{}
+	// newPayment := payment{}
+
+	stripePaymentGw := stripe{}
+	newPayment := payment{
+		gateway: stripePaymentGw,
+	}
+
 	newPayment.makePayment(100.00)
 }
