435d230b04872f7061e0bd7fc3c93141e4080700
alokMIPL <alok@madhulata.com>
2026-09-07 23:23:53 +0530

    code added

--- 022_Enums/main.go
+++ 022_Enums/main.go
@@ -4,10 +4,34 @@ import "fmt"
 
 // enumerated types
 
-func changeOrderStatus(status string) {
+type OrderStatus int
+
+const (
+	Received OrderStatus = iota
+	Confirmed
+	Prepared
+	Delivered
+)
+
+func changeOrderStatus(status OrderStatus) {
 	fmt.Println("Changing order status to", status)
 }
 
+// If we want to use string in enumerated types, we can use the following approach
+type OrderStatusString string
+
+const (
+	ReceivedString  OrderStatusString = "Received"
+	ConfirmedString OrderStatusString = "Confirmed"
+	PreparedString  OrderStatusString = "Prepared"
+	DeliveredString OrderStatusString = "Delivered"
+)
+
+func changeOrderStatusString(status OrderStatusString) {
+	fmt.Println("Changing order status to = ", status, " = by using string enumerated type")
+}
+
 func main() {
-	changeOrderStatus("received")
+	changeOrderStatus(Prepared)
+	changeOrderStatusString(PreparedString)
 }
