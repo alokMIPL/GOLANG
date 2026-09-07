93c9413e754cd5007c39140bf5d7e461c56da5f3
alokMIPL <alok@madhulata.com>
2026-09-08 00:01:47 +0530

    code added

--- 023_Generics/main.go
+++ 023_Generics/main.go
@@ -14,6 +14,14 @@ func printStringSlice(items []string) {
 	}
 }
 
+// See in both func printSlice and printStringSlice, we are doing the same thing, but the only difference is the type of slice. So, we can use generics to avoid this duplication of code.
+
+func printGenericSlice[T any](items []T) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
 func main() {
 
 	// It Print the int Slice
@@ -25,4 +33,11 @@ func main() {
 	// Now Print the string Slice
 	names := []string{"GOLANG", "JS", "SQL"}
 	printStringSlice(names)
+
+	// Now Print the string Slice using generics
+	fmt.Println("*************")
+	printGenericSlice(nums)
+	fmt.Println("*************")
+	// Now Print the string Slice using generics
+	printGenericSlice(names)
 }
