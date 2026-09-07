9b93a88fc90cc82408a16fb84a96e039a718bb68
alokMIPL <alok@madhulata.com>
2026-09-07 23:54:18 +0530

    code added

--- 023_Generics/main.go
+++ 023_Generics/main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+func printSlice(items []int) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
+func printStringSlice(items []string) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
 func main() {
 
+	// It Print the int Slice
+	nums := []int{1, 2, 3}
+	printSlice(nums)
+
+	fmt.Println("*************")
+
+	// Now Print the string Slice
+	names := []string{"GOLANG", "JS", "SQL"}
+	printStringSlice(names)
 }
