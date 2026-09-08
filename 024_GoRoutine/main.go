6b0f1783523417f2845f7ee9e0f5deec3b51cd66
alokMIPL <alok@madhulata.com>
2026-09-08 11:10:28 +0530

    code added

--- 024_GoRoutine/main.go
+++ 024_GoRoutine/main.go
@@ -9,10 +9,22 @@ func task(id int) {
 	fmt.Println("Doing task", id)
 }
 
+func brewCoffee() {
+	fmt.Println("Brewing coffee...")
+}
+
+func toastBread() {
+	fmt.Println("Toasting bread...")
+}
+
 func main() {
 	for i := 0; i <= 10; i++ {
 		go task(i)
 	}
+
+	go brewCoffee()
+	go toastBread()
+	fmt.Println("Breakfast ready!")
 
 	time.Sleep(time.Second * 2)
 
