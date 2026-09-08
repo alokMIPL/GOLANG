bd4fd5f19430ed85584a2bfa55c17be4436587fa
alokMIPL <alok@madhulata.com>
2026-09-08 09:31:12 +0530

    code added

--- 023_Generics/main.go
+++ 023_Generics/main.go
@@ -14,9 +14,20 @@ func printStringSlice(items []string) {
 	}
 }
 
+// ****************
 // See in both func printSlice and printStringSlice, we are doing the same thing, but the only difference is the type of slice. So, we can use generics to avoid this duplication of code.
 
 func printGenericSlice[T any](items []T) {
+	// In place of any we can use interface{} also, but any is more readable and easy to understand.
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
+// Now i want to scope the type so that i can use only int and string in the generic function, so i will create a type constraint for that.
+
+func printGenericScopedSlice[T int | string](items []T) {
+	// We can use int | string to scope the type so that we can use only int and string in the generic function.
 	for _, item := range items {
 		fmt.Println(item)
 	}
@@ -40,4 +51,12 @@ func main() {
 	fmt.Println("*************")
 	// Now Print the string Slice using generics
 	printGenericSlice(names)
+
+	// Now Print the string Slice using generics
+	fmt.Println("*************")
+	printGenericSlice(nums)
+	fmt.Println("*************")
+	// Now Print the string Slice using generics
+	printGenericSlice(names)
+
 }
