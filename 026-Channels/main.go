447611599b5b159001e8e6d933f83bbc4a43b291
alokMIPL <alok@madhulata.com>
2026-09-09 16:27:51 +0530

    code added

--- 026-Channels/main.go
+++ 026-Channels/main.go
@@ -140,4 +140,13 @@ func main() {
 		chan2 <- "GOLANG"
 	}()
 
+	for i := 0; i < 2; i++ {
+		select {
+		case chan1Val := <-chan1:
+			fmt.Println("Received Data from chan1", chan1Val)
+		case chan2Val := <-chan2:
+			fmt.Println("Received Data from chan2", chan2Val)
+		}
+	}
+
 }
