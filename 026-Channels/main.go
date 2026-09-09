cf604ca94cb550c6980104dd7bde7db68f44da75
alokMIPL <alok@madhulata.com>
2026-09-09 15:15:05 +0530

    code added

--- 026-Channels/main.go
+++ 026-Channels/main.go
@@ -120,11 +120,13 @@ func main() {
 	// fmt.Println(<-emailChan)
 	// fmt.Println(<-emailChan)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 5; i++ {
 		emailChan <- fmt.Sprintf("%d@gmail.com", i)
 	}
 
 	fmt.Println("done sending...")
+
+	close(emailChan)
 
 	<-emailDone
 
