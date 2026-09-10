8afec0f6fd6042f9f22970d8aa9ea47e8196cf35
alokMIPL <alok@madhulata.com>
2026-09-10 20:09:14 +0530

    code added

--- 029_Packages/main.go
+++ 029_Packages/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/alok/podcast/auth"
+import (
+	"fmt"
+
+	"github.com/alok/podcast/auth"
+)
 
 // go mod init github.com/alok/podcast
 
 func main() {
 	auth.LoginWithCredentials("alok", "kola")
-
+	session := auth.GetSession()
+	fmt.Println(session)
 }
