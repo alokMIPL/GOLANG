d73db5a691dc0aaf1a8bd39a7701ee6b2f630fd6
alokMIPL <alok@madhulata.com>
2026-09-17 14:42:15 +0530

    code added

--- 031_ADV_GO_LANG/02_Named_Return/main.go
+++ 031_ADV_GO_LANG/02_Named_Return/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+func divide(a int, b int) (john int, sangam int) {
+	john = a / b
+	sangam = a + b
+
+	return
+
+}
+
 func main() {
+	q, r := divide(10, 10)
+
+	fmt.Println("Name Function", q, r)
 
 }
