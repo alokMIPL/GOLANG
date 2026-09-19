d2c26e4f640fcdd25a0a462bdda22b3aa0e40aa5
alokMIPL <alok@madhulata.com>
2026-09-19 16:50:44 +0530

    code added

---
+++ 031_ADV_GO_LANG/09_Methods_Pointer_Receiver/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func main() {
+
+	u := User{Name: "Alok", Age: 24}
+	fmt.Println("Before Methods Pointer Receiver", u.Age)
+
+	u.Birthday()
+
+	fmt.Println("After Methods Pointer Receiver", u.Age)
+
+}
+
+func (u *User) Birthday() {
+	u.Age++
+}
