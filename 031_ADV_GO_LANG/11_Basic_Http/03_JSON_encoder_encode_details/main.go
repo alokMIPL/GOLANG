2e82bf5e81f1fccc053868f1754604ddc82c4613
alokMIPL <alok@madhulata.com>
2026-09-22 23:23:47 +0530

    code added

--- 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/03_JSON_encoder_encode_details/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
+func successHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	res := map[string]any{
+		"ok":       true,
+		"message":  "Json encode succesfully",
+		"datetime": time.Now().UTC(),
+	}
+
+	// encode is writing the JSON and returning back
+	_ = json.NewEncoder(w).Encode(res)
+}
+
+// Decode is read the request form request body
+
 func main() {
+
+	http.HandleFunc("/ok", successHandler)
 
 	err := http.ListenAndServe(":5000", nil)
 	fmt.Println(err)
