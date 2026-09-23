f1555ee825c134c52c1d05e2cd9ff781ac03d4f0
alokMIPL <alok@madhulata.com>
2026-09-23 17:34:41 +0530

    code added

--- 031_ADV_GO_LANG/11_Basic_Http/04_JSON_decoder_decode_details/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/04_JSON_decoder_decode_details/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+func writeJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]any){
+			
+		}
+	}
+}
 
 func main() {
 
