76e37e553579f209dab06477f50d895191c8fc78
alokMIPL <alok@madhulata.com>
2026-09-25 14:44:27 +0530

    code added

---
+++ 031_ADV_GO_LANG/11_Basic_Http/07_Json_Unmarshal_Into_Struct/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	url := "https://catfact.ninja/fact"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println(resp.Status)
+		return
+	}
+}
