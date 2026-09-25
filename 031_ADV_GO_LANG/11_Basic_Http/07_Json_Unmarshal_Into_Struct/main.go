629db364ac356dc1af5163c1b92156d98c06a8a1
alokMIPL <alok@madhulata.com>
2026-09-25 17:09:30 +0530

    code added

--- 031_ADV_GO_LANG/11_Basic_Http/07_Json_Unmarshal_Into_Struct/main.go
+++ 031_ADV_GO_LANG/11_Basic_Http/07_Json_Unmarshal_Into_Struct/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
+
+type CatFactResponse struct {
+	Fact   string `json:"fact"`
+	Length int    `json:"length"`
+}
 
 func main() {
 	url := "https://catfact.ninja/fact"
@@ -22,6 +28,20 @@ func main() {
 		return
 	}
 
+	// It convert JSON BYTES in GO Structs
 	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read body failed", err)
+		return
+	}
+
+	var data CatFactResponse
+
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		fmt.Println("Json Unmarshal failed.")
+		return
+	}
+
+	fmt.Println(data.Fact, data.Length)
 
 }
